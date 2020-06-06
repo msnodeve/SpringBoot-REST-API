24e73fef5dd09b47d6fef19376055fe750dba9bd
msnodeve <msnodeve@gmail.com>
2020-06-07 01:46:25 +0900

    단계별 학습 step02-REST API - getMapping, postMapping, HATEOAS 사용방법 추가

--- src/main/java/seok/controller/TodoController.java
+++ src/main/java/seok/controller/TodoController.java
@@ -1,8 +1,13 @@
 package seok.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import seok.model.Todo;
+import seok.model.TodoResource;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,8 +16,25 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TodoController {
     private final AtomicInteger counter = new AtomicInteger();
 
-    @RequestMapping("/todo")
-    public Todo todo(){
+    @GetMapping("/todo")
+    public Todo findTodo() {
         return new Todo(counter.incrementAndGet(), "코딩하기");
+    }
+
+    @PostMapping("/todo")
+    public Todo registryTodo(@RequestParam String todoTitle) {
+        return new Todo(counter.incrementAndGet(), todoTitle);
+    }
+
+    @PostMapping("/todo/response")
+    public ResponseEntity<Todo> postRegistryTodo(@RequestParam String todoTitle) {
+        return new ResponseEntity<>(new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/todo/hateoas")
+    public ResponseEntity<TodoResource> resourceResponseEntity(@RequestParam String todoTitle) {
+        TodoResource todoResource = new TodoResource(todoTitle);
+        todoResource.add(linkTo(methodOn(TodoController.class).resourceResponseEntity(todoTitle)).withSelfRel());
+        return new ResponseEntity<>(todoResource, HttpStatus.OK);
     }
 }
---
+++ src/main/java/seok/model/TodoResource.java
@@ -0,0 +1,23 @@
+package seok.model;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class TodoResource extends ResourceSupport {
+    private String title;
+
+    public TodoResource() {
+
+    }
+
+    public TodoResource(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
